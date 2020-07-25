da2ec3fe12c3f0953c133181fa719d60789975a3
iMeisa <62470487+iMeisa@users.noreply.github.com>
2020-07-26 00:44:36 +0300

    Reformat and add osu! API

--- Bloatbot.py
+++ Bloatbot.py
@@ -1,7 +1,11 @@
+import discord
 from discord.ext import commands
 from random import randint
-import requests
+from urllib.request import urlopen
+from urllib.parse import urlencode
 import json
+import pickle
+from ratelimit import limits, sleep_and_retry
 
 client = commands.Bot(command_prefix='*')
 
@@ -19,9 +23,9 @@ async def on_message(message):
     elif 'bloatbot' in message.content.lower():
         if message.author.display_name != 'Bloatbot':
             await message.channel.send(':blowfish:')
-    elif 'good bot' in message.content or 'Good bot' in message.content:
+    elif 'good bot' in message.content.lower():
         await message.channel.send(':D')
-    elif 'bad bot' in message.content or 'Bad bot' in message.content:
+    elif 'bad bot' in message.content.lower():
         await message.channel.send('D:')
 
     if message.content.lower() == 'bot':
@@ -31,7 +35,7 @@ async def on_message(message):
     if message.content.lower().startswith('oof'):
         if message.author.display_name != 'Bloatbot':
             await message.channel.send('Oof')
-    if message.content == 'Nice' or '69' in message.content:
+    if message.content.lower() == 'nice' or '69' in message.content:
         if message.author.display_name != 'Bloatbot':
             await message.channel.send('Nice')
     if 'boatbot' in message.content.lower() or message.author.display_name == 'OsuBot':
@@ -149,5 +153,60 @@ async def choose(ctx, *, arg='invalid'):
             await ctx.send(choice2)
     else:
         await ctx.send('Proper format: *choose (choice 1) or (choice 2)')
+
+# osu! API
+with open('osuAPI.pickle', 'rb') as f:
+    api_key = pickle.load(f)
+
+
+@sleep_and_retry
+@limits(calls=60, period=60)
+def call_api(url_param):
+    url = 'https://osu.ppy.sh/api/' + url_param
+    resp = urlopen(url)
+    return json.load(resp)
+
+
+def get_user_id(username):
+    query = urlencode({'k': api_key, 'u': username})
+    user_url = 'get_user' + '?' + query
+    user_data = call_api(user_url)
+    return user_data[0]['user_id']
+
+
+def get_beatmapset_id(beatmap_id):
+    query = urlencode({'k': api_key, 'b': beatmap_id})
+    beatmap_url = 'get_beatmaps' + '?' + query
+    beatmap_data = call_api(beatmap_url)
+    return beatmap_data[0]['beatmapset_id']
+
+
+@client.command()
+async def r(ctx, *, user=''):
+    if len(user) < 1:
+        user = ctx.author.display_name
+    user_pfp = 'https://a.ppy.sh/' + get_user_id(user)
+
+    query = urlencode({'k': api_key, 'u': user, 'type': 'string', 'limit': 1})
+    recent_url = 'get_user_recent' + '?' + query
+    user_recent = call_api(recent_url)
+    beatmap = user_recent[0]
+    beatmap_cover = 'https://assets.ppy.sh/beatmaps/' + get_beatmapset_id(beatmap['beatmap_id']) + '/covers/cover.jpg'
+
+    embed = discord.Embed(
+        title='Title',
+        description='Desc',
+        color=discord.Color.red()
+    )
+
+    embed.set_footer(text='This is a footer')
+    embed.set_image(url='https://images.app.goo.gl/6rSNbPUqK4tNEnvy9')
+    embed.set_thumbnail(url=beatmap_cover)
+    embed.set_author(name=user, icon_url=user_pfp)
+    embed.add_field(name='Field Name', value='Field Value', inline=False)
+    embed.add_field(name='Field Name', value='Field Value', inline=True)
+
+    await ctx.send(embed=embed)
+
 
 client.run('NzA5NTUzMTM4OTc3MjEwMzgx.Xrq7Rw.PwfD4TsmbqdB3vwIcmwf5MBvreU')
