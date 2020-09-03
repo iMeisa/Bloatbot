447e8e597930e873dd5c31a4a6f15cd9bcc68a77
iMeisa <iMeisa@github.com>
2020-09-03 18:37:00 +0300

    mp shows final results

--- BloatbotDev.py
+++ BloatbotDev.py
@@ -45,21 +45,47 @@ async def mp(ctx, match_link):
     match_title = match_data['match']['name']
     match_games = match_data['games']
 
-    embed = discord.Embed(
-        title=match_title,
-        url=match_link,
-        colour=discord.Colour.magenta()
-    )
-
+    match_scores = []
+    player_scores = {}
     user_ids = {}
     for game in match_games:
         game_scores = game['scores']
-        for score in game_scores:
-            user_id = score['user_id']
-            if user_id not in user_ids:
-                score_username = get_user_data(user_id)['username']
-                user_ids[user_id] = score_username
-            embed.add_field(name=user_ids[user_id], value=score['score'], inline=True)
+
+        score1 = game_scores[0]
+        score2 = game_scores[1]
+        user_id1 = score1['user_id']
+        user_id2 = score2['user_id']
+
+        if user_id1 not in player_scores:
+            player_scores[user_id1] = 0
+            player_scores[user_id2] = 0
+            score1_username = get_user_data(user_id1)['username']
+            user_ids[user_id1] = score1_username
+            score2_username = get_user_data(user_id2)['username']
+            user_ids[user_id2] = score2_username
+
+        player1_score = score1['score']
+        player2_score = score2['score']
+        if player1_score > player2_score:
+            player_scores[user_id1] += 1
+        else:
+            player_scores[user_id2] += 1
+
+    player1_id = list(user_ids.keys())[0]
+    player2_id = list(user_ids.keys())[1]
+    if player_scores[player1_id] > player_scores[player2_id]:
+        final_score = f'**{user_ids[player1_id]} {player_scores[player1_id]}** | ' \
+                      f'{player_scores[player2_id]} {user_ids[player2_id]}'
+    else:
+        final_score = f'{user_ids[player1_id]} {player_scores[player1_id]} | ' \
+                      f'**{player_scores[player2_id]} {user_ids[player2_id]}**'
+
+    embed = discord.Embed(
+        title=match_title,
+        url=match_link,
+        colour=discord.Colour.magenta(),
+        description=final_score
+    )
 
     thumbnail = 'https://cdn.discordapp.com/attachments/734824448137625731/734824581080285265/TheLogoFinal.png'
     embed.set_thumbnail(url=thumbnail)
