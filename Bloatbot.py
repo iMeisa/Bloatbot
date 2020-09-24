8a9e98e158798a5027576f8a7bb9e2657a9e3994
iMeisa <iMeisa@github.com>
2020-09-24 11:42:30 +0300

    strikethrough pp for failed plays

--- Bloatbot.py
+++ Bloatbot.py
@@ -612,7 +612,9 @@ def create_play_embed(user, beatmap_id=None, channel_id=None, beatmap_only=False
                                  max_combo=beatmap['maxcombo'], miss_count=n0)
     pp_max = pp_calculation(beatmap_id, mods=compressed_mods)
 
-    if beatmap_rank_status in ['2', '1']:
+    if beatmap['rank']:
+        pp_value = f'~~**{pp_achieved}pp**/{pp_max}PP~~'
+    elif beatmap_rank_status in ['2', '1']:
         pp_value = f'**{pp_achieved}pp**/{pp_max}PP'
     else:
         pp_value = f'~~**{pp_achieved}pp**/{pp_max}PP~~'
