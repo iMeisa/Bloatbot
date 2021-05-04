e991b2fce4fa4f7b92100ffb97332326a17cfb21
iMeisa <iMeisa@github.com>
2021-05-04 19:12:57 +0300

    Update new recent command to work with registration

--- cogs/Recent.py
+++ cogs/Recent.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 from util import osu
+import json
 
 
 class Recent(commands.Cog):
@@ -9,7 +10,15 @@ class Recent(commands.Cog):
 
     @commands.command()
     async def nr(self, ctx):
-        user = 'Meisa'  # While testing
+        with open('cache/users.json', 'r') as f:
+            users = json.load(f)
+
+        author = str(ctx.author.id)
+        if author not in list(users.keys()):
+            await ctx.send('Who is you? Tell me who you are by doing *register `[your osu username]`')
+            return
+        user = json.load(f)[str(ctx.author.id)]
+
         embed = osu.create_play_embed(user, channel_id=ctx.channel.id)
 
         if isinstance(embed, str):
