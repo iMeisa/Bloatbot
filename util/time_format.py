00cc59f430459875fe21097486d5b498f14872c8
iMeisa <iMeisa@github.com>
2021-05-06 13:26:02 +0300

    Updated time format

--- util/time_format.py
+++ util/time_format.py
@@ -9,36 +9,36 @@ def get_time_diff(time_origin):
     if time_diff.days < 1:
         if time_diff.seconds >= 3600:
             hours = time_diff.seconds // 3600
-            if hours > 1:
+            if hours >= 2:
                 return f'{hours} hours ago'
             else:
                 return '1 hour ago'
         elif time_diff.seconds >= 60:
             minutes = time_diff.seconds // 60
-            if minutes > 1:
+            if minutes >= 2:
                 return f'{minutes} minutes ago'
             else:
                 return '1 minute ago'
         else:
-            if time_diff.seconds > 1:
+            if time_diff.seconds >= 2:
                 return f'{time_diff.seconds} seconds ago'
             else:
                 return '1 second ago'
     else:
         if time_diff.days >= 365:
             years = time_diff.days // 365
-            if time_diff.days > 1:
+            if years >= 2:
                 return f'{years} years ago'
             else:
                 return '1 year ago'
         elif time_diff.days >= 30:
             months = time_diff.days // 30
-            if months > 1:
+            if months >= 2:
                 return f'{months} months ago'
             else:
                 return '1 month ago'
         else:
-            if time_diff.days > 1:
+            if time_diff.days >= 2:
                 return f'{time_diff.days} days ago'
             else:
                 return '1 day ago'
