df26c1b5defa94f82b11542ed2d57a52a7981603
iMeisa <iMeisa@github.com>
2021-05-20 14:48:18 +0300

    Added *c [beatmap link]

--- cogs/Compare.py
+++ cogs/Compare.py
@@ -12,8 +12,18 @@ class Compare(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def c(self, ctx, username=None):
+    async def c(self, ctx, *args):
         user_id = None
+        username = None
+        beatmap_id = None
+
+        for arg in args:
+            if arg.startswith('https://osu.ppy.sh/b'):
+                beatmap_id = arg.split('/')[-1]
+                add_recent_beatmap(ctx.channel.id, beatmap_id)
+            else:
+                username = arg
+
         if username is None:
             user_id = get_registered_user(ctx.author.id)
             if user_id is None:
@@ -22,7 +32,7 @@ class Compare(commands.Cog):
 
         user = get_user(username) if username is not None else get_user(user_id, is_id=True)
 
-        beatmap_id = get_recent_beatmap(ctx.channel.id)
+        beatmap_id = get_recent_beatmap(ctx.channel.id) if beatmap_id is None else beatmap_id
         if beatmap_id is None:
             await ctx.send("No beatmaps have been posted on this channel yet")
             return
@@ -32,7 +42,7 @@ class Compare(commands.Cog):
             await ctx.send(f"{user.name} hasn't passed this map yet")
             return
 
-        add_recent_beatmap(ctx.channel.id, score.beatmap_id)
+        add_recent_beatmap(ctx.channel.id, beatmap_id)
 
         # Embed
         embed = create_score_embed(user, score)
